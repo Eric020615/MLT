5fdfbe84ecccafd603e5e38890e4a2a9639b4c18
JUN CHENG <ericjuncheng10@gmail.com>
2022-06-07 18:01:11 +0800

    Update to details

---
+++ src/main/java/com/example/mlt/UserProfilePageController.java
@@ -0,0 +1,40 @@
+package com.example.mlt;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class UserProfilePageController {
+
+    @FXML
+    private PasswordField ConfirmPasswordEntered;
+
+    @FXML
+    private TextField ContactNumberEntered;
+
+    @FXML
+    private TextField EmailEntered;
+
+    @FXML
+    private PasswordField NewPasswordEntered;
+
+    @FXML
+    private TextField UsernameEntered;
+
+    @FXML
+    void BackHomePageButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DeleteButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SaveButton(ActionEvent event) {
+
+    }
+
+}
